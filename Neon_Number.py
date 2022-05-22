fbb410a9f5e05525d60e0b2222856146ba821554
Mehedi Mostazir <97426129+Mehedi-mostazir@users.noreply.github.com>
2022-05-22 16:17:38 +0530

    n = int(input()) sq_n = n*n sum=0 while sq_n:     sum+=sq_n%10     sq_n//=10 if sum==n:     print("Neon Number") else:     print("Not Neon Number")

--- Neon_Number.py
+++ Neon_Number.py
@@ -1,10 +1,10 @@
-n=int(input())
-sq=n*n
+n = int(input())
+sq_n = n*n
 sum=0
-while sq>0:
-    sum=sum+(sq%10)
-    sq=sq//10
+while sq_n:
+    sum+=sq_n%10
+    sq_n//=10
 if sum==n:
     print("Neon Number")
 else:
-    print('Not Neon Number')
+    print("Not Neon Number")
