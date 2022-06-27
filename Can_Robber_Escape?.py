068c1ac6b447c47c0af9d64226054a18bc7495ee
Mehedi Mostazir <97426129+Mehedi-mostazir@users.noreply.github.com>
2022-06-27 21:54:11 +0530

    n=int(input()) arr=list(map(int,input().split())) f=0 for i in arr:     if i<n:         f=1     else:         f=0         break if f==1:     print("YES") else:     print("NO")

---
+++ Can_Robber_Escape?.py
@@ -0,0 +1,13 @@
+n=int(input())
+arr=list(map(int,input().split()))
+f=0
+for i in arr:
+    if i<n:
+        f=1
+    else:
+        f=0
+        break
+if f==1:
+    print("YES")
+else:
+    print("NO")
