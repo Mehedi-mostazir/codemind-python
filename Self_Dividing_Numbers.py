93cc2d53a009f4a841f9417b6d7c4a59fceb0998
Mehedi Mostazir <97426129+Mehedi-mostazir@users.noreply.github.com>
2022-07-09 08:40:42 +0530

    a = int(input()) b = int(input()) while a<=b:     temp = a     di = 0     d = 0     while temp:         d += 1         if temp%10!=0 and a%(temp%10)==0:             di += 1         temp//=10     if di==d:         print(a,end=" ")     a += 1

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -1,15 +1,14 @@
-a=int(input())
-b=int(input())
-for i in range(a,b+1):
-    count=0
-    count1=0
-    temp=i
-    while(temp>0):
-        r=temp%10
-        count1+=1
-        if(r>0):
-            if(i%r==0):
-                count+=1
-        temp=temp//10
-    if(count==count1):
-        print(i,end=' ')
+a = int(input())
+b = int(input())
+while a<=b:
+    temp = a
+    di = 0
+    d = 0
+    while temp:
+        d += 1
+        if temp%10!=0 and a%(temp%10)==0:
+            di += 1
+        temp//=10
+    if di==d:
+        print(a,end=" ")
+    a += 1
